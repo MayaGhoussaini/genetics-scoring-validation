168a9e73a093c9c59ded487271906890a658827c
edm1 <emountjoy@gmail.com>
2019-02-01 17:15:05 +0000

    clean up

--- 2_get_v2g_evidence.py
+++ 2_get_v2g_evidence.py
@@ -36,46 +36,6 @@ def main():
 
     return 0
 
-def process_raw(inf, client):
-    ''' Loads and cleans the raw into validation set
-    Params:
-        inf (str): input file
-        client (OT_Genetics)
-    Returns:
-        df
-    '''
-    # Load
-    df = pd.read_csv(inf, sep='\t', header=0)
-
-    # Make assertations
-    assert (df.rsid.str.startswith('rs')).all()
-    assert (df.condifence.isin(['High', 'Medium', 'Low'])).all()
-    assert ~(pd.isnull(df.rsid) & pd.isnull(df.varid)).all()
-    assert ~pd.isnull(df.loc[:, ['set', 'source', 'gene_id', 'condifence']]).any(axis=None)
-
-    # Find unkown variant IDs
-    is_unknown = pd.isnull(df.varid)
-    rsids_to_get = df.rsid[is_unknown]
-    rsid_map = client.rsid_to_varid(rsids_to_get.tolist())
-    df.loc[is_unknown, 'varid'] = df.loc[is_unknown, 'rsid'].apply(
-        lambda rsid: rsid_map.get(rsid, [])
-    )
-    df['varid_ambiguous'] = (df.varid.str.len() > 1)
-
-    # Explode lines
-    df = explode(df, ['varid'])
-
-    return df
-
-def explode(df, columns):
-    ''' Explodes multiple columns
-    '''
-    idx = np.repeat(df.index, df[columns[0]].str.len())
-    a = df.T.reindex(columns).values
-    concat = np.concatenate([np.concatenate(a[i]) for i in range(a.shape[0])])
-    p = pd.DataFrame(concat.reshape(a.shape[0], -1).T, idx, columns)
-    return pd.concat([df.drop(columns, axis=1), p], axis=1).reset_index(drop=True)
-
 if __name__ == '__main__':
 
     main()
