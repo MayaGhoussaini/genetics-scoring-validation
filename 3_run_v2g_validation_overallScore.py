86188bb39e77f2e0378c52010d444efbbb6aa83c
Edward Mountjoy <emountjoy@gmail.com>
2019-02-02 10:08:43 +0000

    finished plot

--- 3_run_v2g_validation_overallScore.py
+++ 3_run_v2g_validation_overallScore.py
@@ -7,6 +7,12 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
+import seaborn as sns; sns.set()
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 def main():
 
@@ -53,12 +59,45 @@ def main():
     data = data.loc[data.gold_standard_gene_id == data.gene_id, :]
     print('N variants after v2g merge: ', gs.shape[0])
 
-    data.to_csv('data.temp.tsv', sep='\t', index=None)
-
+    # Make plot
+    out_plot = os.path.join(out_dir, 'cumulative.plot.png')
+    plot_cumulative_curve(data.normalisedScore, out_plot)
 
     # Write
-    # outf = os.path.join(out_dir, 'evidence.tsv')
-    # res.to_csv(outf, sep='\t', index=None)
+    outf = os.path.join(out_dir, 'evidence.tsv')
+    data.to_csv(outf, sep='\t', index=None)
+
+    return 0
+
+def plot_cumulative_curve(s, outf):
+    ''' Plot score on x-axis and % of gold standards achieving score on Y-axis
+    Params:
+        s (pd.series): series of scores
+        outf (file): file to output plot to
+    '''
+    # Create data
+    x_list = np.linspace(0, 1, 21)
+    y_list = [ ((s >= x).sum() * 100 / s.size) for x in x_list ]
+
+    # Make plot
+    ax = sns.lineplot(x=x_list, y=y_list)
+
+    # Increase tick frequency
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+    ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+
+    # Style
+    sns.set(style='whitegrid')
+    ax.set(
+        xlabel='Normalised score (score / max(score))',
+        ylabel='% Gold-standards ≥ Normalised score',
+        title='V2G gold-standard validation'
+    )
+
+    # Save
+    ax.get_figure().savefig(outf)
 
     return 0
 
